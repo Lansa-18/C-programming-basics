3c4668fb8718ced60228cab7a0a0546308a87c89
Mapelujo <olamideiyanda18@gmail.com>
2025-01-27 21:58:53 +0100

    understanding variables

--- hello.c
+++ hello.c
@@ -2,8 +2,35 @@
 
 int main() {
     printf("Hello World!\n");
+
+    int age = 21;
+    printf("Age: %d", age);
+
+    age = 31;
+    printf("\nNew age: %d\n", age);
+
+    int firstNumber = 33;
+    printf("First Number = %d\n", firstNumber);
+
+    int secondNumber = firstNumber;
+    printf("Second Number = %d\n", secondNumber);
+
+    int variable1, variable2 = 25;
+
     return 0;
 }
 
 // VARIABLES
 // A variable is a name given to a memory location inside our computer where we can store data.
+// Variable Naming Convention: Use good descriptive variable names, follow the camelCase format
+/* 
+    Rules to follow when creating variale names
+    - You ccannot create variable names with space in between.
+    - You cannot start variable names with numbers.
+    - You cannot use certain words as variable names. e.g if
+*/
+
+/*
+    DATA TYPES
+
+*/
