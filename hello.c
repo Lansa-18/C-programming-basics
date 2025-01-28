d742700cb071624cc05749796cc33566a76c184d
Mapelujo <olamideiyanda18@gmail.com>
2025-01-28 05:50:39 +0100

    understanding data types

--- hello.c
+++ hello.c
@@ -17,6 +17,20 @@ int main() {
 
     int variable1, variable2 = 25;
 
+    double result = 4.5;
+    float standardScore = 5.0f;
+
+    printf("My CGPA is: %.2lf\n", result);
+    printf("The score %.2f is a first class result\n", standardScore);
+
+    // Storing an exponential number
+    double eNum = 5.5e6;
+    printf("%lf\n", eNum);
+
+    char character = 'z';
+    printf("%c\n", character);
+    printf("%d\n", character);
+
     return 0;
 }
 
@@ -32,5 +46,16 @@ int main() {
 
 /*
     DATA TYPES
+    A size of a variable is usually 4-bytes
+
+    int         (4-bytes) | %d for printing
+    double      (8-bytes) | %lf for printing  This takes 6 digits after the decimal point is printed.
+    float       (4-bytes) | %f for printing
+    char        (1-byte)  | %c for printing
+
+    It is important to note that characters are internally stored as integers.
+
+    sizeof      %zu for printing
+
 
 */
