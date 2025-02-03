93435d81dc39e266f99f045835d7f20d91417027
Mapelujo <olamideiyanda18@gmail.com>
2025-02-03 04:02:10 +0100

    understanding if..else statements

--- hello.c
+++ hello.c
@@ -69,12 +69,16 @@ int main() {
     // printf("\n%d ", value3);
 
     int age;
-    age = 25;
+    age = 22;
 
     // printf("Enter your age: ");
     // scanf("%d", &age);
 
-    if (age >= 18) {
+    if (age > 120) {
+        printf("Invalid age");
+    } else if (age < 0) {
+        printf("Invalid age");
+    } else if (age >= 18) {
         printf("You are eligible to vote.");
     } else {
         printf("Sorry, you are not eligible to vote.");
